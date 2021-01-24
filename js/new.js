c68e81d7c03c1cf54cea02ee0a918088547f15c9
Dawid Stasiak <dawid.stasiak17@gmail.com>
2021-01-24 20:26:20 +0100

    New invoice frontend wip

---
+++ js/new.js
@@ -0,0 +1,14 @@
+number_of_rows = 0;
+
+const new_row = () =>{
+    table = document.getElementById("items");
+    table.innerHTML += `<td id="row${number_of_rows}"><input type="text" name="name_of_item" placeholder="Nazwa towaru lub usługi"></td><button id="button${number_of_rows}" onclick="remove_row(${number_of_rows})" type="button">- Usuń wiersz</button>`;
+    number_of_rows++;  
+}
+
+const remove_row = (row_num) =>{
+    row = document.getElementById("row" + row_num);
+    button = document.getElementById("button" + row_num);
+    button.remove();
+    row.remove();
+}
