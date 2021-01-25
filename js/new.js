2aaf9c32472d18103d1696f7c8e6205764e9e9d8
Dawid Stasiak <dawid.stasiak17@gmail.com>
2021-01-25 10:36:37 +0100

    Code cleanup

--- js/new.js
+++ js/new.js
@@ -1,14 +1,24 @@
 number_of_rows = 0;
 
 const new_row = () => {
-    table = document.getElementById("items");
-    table.innerHTML += `<tr id="row${number_of_rows}"><td ><input type="text" name="name_of_item" placeholder="Nazwa towaru lub usługi"></td><td><input type="number" placeholder="ilosc"></td><td><input type="text" placeholder="jednostka"></td><td><input type="number" placeholder="cena brutto"></td><td><input type="number" placeholder="Rabat (%)"></td><td>Po rabacie : <div id="after_discount${number_of_rows}"></div></td><td><select><option>zw.</option><option>0%</option><option>5%</option><option>8%</option><option>23%</option></select></td></tr><button id="button${number_of_rows}" onclick="remove_row(${number_of_rows})" type="button">- Usuń wiersz</button>`;
+    const table = document.getElementById("items");
+    table.innerHTML +=
+        `<tr id="row${number_of_rows}">
+            <td ><input type="text" name="name_of_item" placeholder="Nazwa towaru lub usługi"></td>
+            <td><input type="number" placeholder="ilosc"></td>
+            <td><input type="text" placeholder="jednostka"></td>
+            <td><input type="number" placeholder="cena brutto"></td>
+            <td><input type="number" placeholder="Rabat (%)"></td>
+            <td>Po rabacie : <div id="after_discount${number_of_rows}"></div></td>
+            <td><select><option>zw.</option><option>0%</option><option>5%</option><option>8%</option><option>23%</option></select></td>
+        </tr>
+        <button id="button${number_of_rows}" onclick="remove_row(${number_of_rows})" type="button">- Usuń wiersz</button>`;
     number_of_rows++;  
 }
 
 const remove_row = (row_num) => {
-    row = document.getElementById("row" + row_num);
-    button = document.getElementById("button" + row_num);
+    const row = document.getElementById("row" + row_num);
+    const button = document.getElementById("button" + row_num);
     button.remove();
     row.remove();
 }
--- php/create_invoice.php
+++ php/create_invoice.php
@@ -1,1 +1,2 @@
 <?php
+echo "<pre><h1>Wysłano</h1></pre>";
