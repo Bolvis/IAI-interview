f88c9420b558fc6a239ab8b848dcbf66219ca50b
Dawid Stasiak <dawid.stasiak17@gmail.com>
2021-01-25 23:25:32 +0100

    Print all invoices

--- php/list.php
+++ php/list.php
@@ -1,2 +1,13 @@
 <?php
-echo "<h1 style='text-align:center;'>Tutaj będzie lista faktur</h1>";
+$con = mysqli_connect('localhost', 'root', '','iai');
+$get_all_invoices = "SELECT nr, company_name, date_of_issue FROM invoices INNER JOIN customers ON invoices.customer_id = customers.id";
+$all_invoices = mysqli_query($con, $get_all_invoices);
+
+echo "<table border='1'>";
+while ($row = mysqli_fetch_row($all_invoices)){
+    echo "<tr>";
+    foreach ($row as $value){
+        echo "<td>$value</td>";
+    }
+    echo "<td><button>download pdf</button></td></tr>";
+}
